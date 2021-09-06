913501b940adee188799e11885f2e14ba8b90f9b
David.Lacoste <david.lacoste2@gmail.com>
2021-09-06 16:36:40 +0200

    Put db host env in docker-compose

--- config/mongo-config.js
+++ config/mongo-config.js
@@ -1,6 +1,6 @@
 const {MongoClient} = require('mongodb');
 
-const uri = "mongodb://root:password@localhost:27017";
+const uri = process.env.MONGO_URI || "mongodb://root:password@localhost:27017";
 const client = new MongoClient(uri);
 
 async function connect() {

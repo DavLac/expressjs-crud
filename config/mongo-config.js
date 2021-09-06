d369657287b53a0531bd969f7654d36219c85874
David.Lacoste <david.lacoste2@gmail.com>
2021-09-06 13:21:35 +0200

    Add user CRUD

--- config/mongo-config.js
+++ config/mongo-config.js
@@ -1,27 +1,30 @@
 const {MongoClient} = require('mongodb');
 
-async function main() {
-    const uri = "mongodb://localhost:27017";
+const uri = "mongodb://root:password@localhost:27017";
+const client = new MongoClient(uri);
 
-    const client = new MongoClient(uri);
-
+async function connect() {
     try {
         // Connect to the MongoDB cluster
         await client.connect();
 
     } catch (e) {
         console.error(e);
-    } finally {
-        await client.close();
     }
+}
+
+function getUserCollection() {
+    const db = client.db('main');
+    return db.collection('users');
 }
 
 module.exports = {
     initDb: function () {
-        main()
+        connect()
             .then(client => {
-                console.log('Connected to Database')
+                console.log('Connected to Database');
             })
             .catch(console.error);
-    }
+    },
+    userCol: getUserCollection
 };
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,41 @@
 var express = require('express');
 var router = express.Router();
+var config = require('../config/mongo-config');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/:username', function (req, res, next) {
+    config.userCol().findOne({username: req.params.username})
+        .then((users) => {
+            res.json(users)
+        });
 });
+
+router.get('/', function (req, res, next) {
+    config.userCol().find().toArray()
+        .then((users) => {
+            res.json(users)
+        });
+});
+
+router.post('/', function (req, res, next) {
+    config.userCol().insertOne(req.body);
+    res.json(req.body);
+})
+
+router.put('/:username', function (req, res, next) {
+    config.userCol().findOneAndUpdate(
+        {username: req.params.username},
+        {
+            $set: {
+                username: req.body.username,
+                password: req.body.password
+            }
+        });
+    res.json(req.body);
+})
+
+router.delete('/:username', function (req, res, next) {
+    config.userCol().findOneAndDelete({username: req.params.username});
+    res.json(req.body);
+})
 
 module.exports = router;
